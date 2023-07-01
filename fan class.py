025292549d340aa67c5352800026802dbc561137
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 18:44:40 +0800

    pseudocode for class fan

---
+++ fan class.py
@@ -0,0 +1,10 @@
+# Designing a class named Fan to represent a fan
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# constants for fan speeds
+# constructor for initializing the Fan object
+# getter and setter methods for speed
+# for radius
+# for color
+# for on
