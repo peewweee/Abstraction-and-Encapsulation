a3f471a6597278552c039cea0d311c5f2848180f
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 20:45:33 +0800

    pseudocode for car class

---
+++ car_class.py
@@ -0,0 +1,9 @@
+# Car Class to display current speed of car
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# class car
+# initialize car object (speed attribute = 0)
+# accelerate method
+# brake method
+# get_speed method
