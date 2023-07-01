8b83f9a51d063f129e61ac8ebf183d6888a2af22
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-02 01:21:55 +0800

    pseudocode for pet class

---
+++ pet_class.py
@@ -0,0 +1,13 @@
+# Pet Class
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# class pet
+# initialize attributes
+
+# set_name method
+# set_animal_type method
+# set_age method
+# get_name method
+# get_animal_type method
+# get_age method
