5647b5509a0c095688165d36b0eeddd0c69c56c9
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-02 01:51:48 +0800

    pet object and input prompts

--- PetObject.py
+++ PetObject.py
@@ -2,8 +2,15 @@
 # Phoebe Rhone L. Gangoso | BSCpE 1-4
 
 # PSEUDOCODE
+from pet_class import Pet
 # create object of the Pet class
+pet = Pet ()
+
 # input prompts (name, animal type, and age)
+name = input("Enter your pet's name: ")
+animal_type = input("Enter your pet's animal type: ")
+age = input("Enter your pet's age: ")
+
 # set as object attributes
 # retrieve name, type, and age
 # display data
