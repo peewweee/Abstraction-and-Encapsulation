e6ab81d65829e17ef0eb87cb1dc12927ad8fa213
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 21:33:29 +0800

    added methods

--- car_class.py
+++ car_class.py
@@ -9,6 +9,15 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
     # accelerate method
-# brake method
-# get_speed method
+    def accelerate(self):
+        self.__speed += 5
+
+    # brake method
+    def brake(self):
+        self.__speed -= 5
+
+    # get_speed method
+    def get_speed(self):
+        return self.__speed
