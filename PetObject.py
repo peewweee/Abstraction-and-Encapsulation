bf46ba4e2eebd8d157310e801b602fa93ec59c56
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-02 01:45:52 +0800

    pseudocode for pet object

---
+++ PetObject.py
@@ -0,0 +1,9 @@
+# Pet Object
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# create object of the Pet class
+# input prompts (name, animal type, and age)
+# set as object attributes
+# retrieve name, type, and age
+# display data
