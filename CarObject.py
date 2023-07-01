b106c8c11b8f67e1c84fac67dbe76f536094ec7c
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 21:38:04 +0800

    pseudocode for main program

---
+++ CarObject.py
@@ -0,0 +1,8 @@
+# Main program to display current speed of car
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# import car class
+# create car object
+# call accelerate method 5 times
+# call brake method 5 times
