1756d5b96d0f7678c5aee4460f6d7e0a842b79ec
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 19:40:33 +0800

    fan 1 is working

--- TestFan.py
+++ TestFan.py
@@ -11,7 +11,16 @@ class TestFan:
         # two Fan objects
         fan_1 = Fan(Fan.FAST, 10, 'yellow', True)
         fan_2 = Fan(Fan.MEDIUM, 5, 'blue', False)
-# print fan1
+
+        # print fan1
+        print("Fan 1:")
+        print("Speed:", fan_1.get_speed())
+        print("Radius:", fan_1.get_radius())
+        print("Color:", fan_1.get_color())
+        print("On:", fan_1.is_on())
+
 # print fan2
 
 # instance of TestFan and run the test
+test = TestFan()
+test.run_test()
