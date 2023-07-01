7759adb2d5ac6b23456035c247e7c34735474ff2
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 18:58:19 +0800

    initializing values for object

--- fan class.py
+++ fan class.py
@@ -2,8 +2,17 @@
 # Phoebe Rhone L. Gangoso | BSCpE 1-4
 
 # PSEUDOCODE
-# constants for fan speeds
-# constructor for initializing the Fan object
+class Fan:
+    # constants for fan speeds
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+    # constructor for initializing the Fan object
+    def __init__(self, speed=1, radius=5, color='blue', on=False):
+        self.__speed = speed
+        self.__on = on
+        self.__radius = radius
+        self.__color = color
 # getter and setter methods for speed
 # for radius
 # for color
