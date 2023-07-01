19b1612ff20e3ac972068ff5dcd2ca715c986c50
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 21:24:59 +0800

    class car

--- car_class.py
+++ car_class.py
@@ -3,7 +3,12 @@
 
 # PSEUDOCODE
 # class car
-# initialize car object (speed attribute = 0)
-# accelerate method
+class Car:
+    def __init__(self, year_model, make):
+    # initialize car object (speed attribute = 0)
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+    # accelerate method
 # brake method
 # get_speed method
