e62c2acc169e9119ff8dc896986b550770db9105
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 22:28:49 +0800

    accelerate method

--- CarObject.py
+++ CarObject.py
@@ -7,4 +7,7 @@ from car_class import Car
 # create car object
 car_sample = Car(2023, "Toyota")
 # call accelerate method 5 times
+for _ in range(5):
+    car_sample.accelerate()
+    print("Current speed:", car_sample.get_speed())
 # call brake method 5 times
