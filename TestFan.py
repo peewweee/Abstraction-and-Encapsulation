dd88b20b9e4a11c06b7b3f74d618448c2ef18624
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-01 19:22:52 +0800

    pseudocode for TestFan.py

---
+++ TestFan.py
@@ -0,0 +1,11 @@
+# Designing a class named Fan to represent a fan
+# Phoebe Rhone L. Gangoso | BSCpE 1-4
+
+# PSEUDOCODE
+# import class fan
+# create class TestFan
+# two Fan objects
+# print fan1
+# print fan2
+
+# instance of TestFan and run the test
